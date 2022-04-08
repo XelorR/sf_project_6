6e2cca3b4a1dc5d32d407dee4cf18f2cc39c32e8
Petr <petr@duck.com>
2022-04-09 00:10:50 +0300

    model related functions separated to external file

---
+++ lib/model_related.py
@@ -0,0 +1,27 @@
+import imp
+
+
+import os
+
+import joblib
+import pandas as pd
+import numpy as np
+
+
+def train_or_load(clf, X, y, filepath: str, complevel=9):
+    if os.path.exists(filepath):
+        with open(filepath, "rb") as f:
+            clf = joblib.load(f)
+    else:
+        clf.fit(X, y)
+        with open(filepath, "wb") as f:
+            joblib.dump(clf, f, compress=complevel)
+    return clf
+
+
+def get_dumb_dummies(df: pd.DataFrame) -> pd.DataFrame:
+    return pd.get_dummies(df, columns=df.select_dtypes("object").columns.tolist())
+
+
+def mape(y_true, y_pred):
+    return np.mean(np.abs((y_pred - y_true) / y_true))
